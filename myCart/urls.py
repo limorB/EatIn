a3512da0a4bfbfe6d4d8ba2788ea96029fc72f7b
Limor Ben Susen <limorbensusen@Limors-MacBook-Air.local>
2019-09-25 17:11:59 +0300

    remove from cart with cart.id

--- myCart/urls.py
+++ myCart/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.display_cart, name='display_cart'),
     # path('', views.remove_from_cart, name='remove_from_cart'),
     # path('remove', views.remove_from_cart, name='remove_from_cart'),
-    path('remove/<int:id>', views.remove_from_cart, name='remove_from_cart')
+    path('remove/<int:id>', views.remove_from_cart, name='remove_from_cart'),
+    path('update/<int:id>', views.update_quantity, name='update_quantity')
 
 ]
--- myCart/views.py
+++ myCart/views.py
@@ -12,27 +12,23 @@ def display_cart(request):
     if request.method == 'GET':
         user_id = request.user.id
         carts = Cart.objects.filter(user_id=user_id)
-        all_foods_list = []
-        if len(carts)>0:
-            for cart in carts:
-                food = Food.objects.get(pk = cart.food_id)
-                print(food.description)
-                all_foods_list.append(food)
-        else:
-            print("your cart is empty")
+        if len(carts) == 0:
             messages.info(request, "your cart is empty")
-            return render(request, 'mycart/index.html')
 
-
-    return render(request, 'mycart/index.html',{'foods':all_foods_list})
+    return render(request, 'mycart/index.html',{'carts':carts})
 
 
 @login_required(login_url='/accoutns/login/')
 def remove_from_cart(request,id):
-    #notice the id is a food_id and not cart_id
     if request.method == 'GET':
-        print("this is a get")
         user_id = request.user.id
-        cart = Cart.objects.get(user_id=user_id,food_id=id)
+        cart = Cart.objects.get(pk=id)
         cart.delete()
     return display_cart(request)
+
+# @login_required(login_url='/accoutns/login/')
+def update_quantity(request,id):
+    if request.method == 'GET':
+        print(id)
+
+        return request
