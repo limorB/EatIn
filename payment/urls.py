3155e705ecc2c28a29a8c0582bc3db59e49ef07c
Limor Ben Susen <limorbensusen@Limors-MacBook-Air.local>
2019-09-28 18:18:16 +0300

    order model added,cart model updated

--- payment/urls.py
+++ payment/urls.py
@@ -1,7 +1,8 @@
 from . import views
-from django.urls import path
+from django.urls import path,include
 
 
 urlpatterns = [
     path('', views.display_checkout, name='display_checkout'),
+    path('paypal/', include('paypal.standard.ipn.urls')),
 ]
--- payment/views.py
+++ payment/views.py
@@ -4,3 +4,26 @@ from django.http import HttpResponse
 
 def display_checkout(request):
     return render(request, 'payment/index.html')
+
+# def process_payment(request):
+#     order_id = request.session.get('order.id')
+#     order = get_object_or_404(Order, id=order_id)
+#     host = request.get_host()
+#
+#     paypal_dict = {
+#         'business': settings.PAYPAL_RECEIVER_EMAIL,
+#         'amount': '%.2f' % order.total_cost().quantize(
+#             Decimal('.01')),
+#         'item_name': 'Order {}'.format(order.id),
+#         'invoice': str(order.id),
+#         'currency_code': 'USD',
+#         'notify_url': 'http://{}{}'.format(host,
+#                                            reverse('paypal-ipn')),
+#         'return_url': 'http://{}{}'.format(host,
+#                                            reverse('payment_done')),
+#         'cancel_return': 'http://{}{}'.format(host,
+#                                               reverse('payment_cancelled')),
+#     }
+#
+#     form = PayPalPaymentsForm(initial=paypal_dict)
+#     return render(request, 'ecommerce_app/process_payment.html', {'order': order, 'form': form})
